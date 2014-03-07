80e2654ef880f7afafb35469e95fe886f0b7cbaf
cScarlson <otocarlson@gmail.com>
2014-03-07 11:57:14 -0700

    created module

--- Rally/RallyTechTest-BoardGame.js
+++ Rally/RallyTechTest-BoardGame.js
@@ -66,90 +66,181 @@ var people = [
     {name: 'FFF', alive: true}
 ];
 
+var Darwin = (function(){
 
-'use strict';
-var BOARDX, BOARDY, AREA, MATROID, PARTICIPANTS, MEMBERS;
+  'use strict';
+  var BOARDX, BOARDY, AREA, MATROID, PARTICIPANTS, MEMBERS = [];
 
-function getNeighbor(x, y, dir){
-    //console.log('@MATROID[x][y]',x,y);
+  function getNeighborCoords(x, y, dir){
     var NE = 1, SW = -1;
     var neighbor = undefined;
     if(dir === 'North'){
-        neighbor = MATROID[x] ? MATROID[x][y+NE] : undefined;
+        neighbor = (MATROID[x] && MATROID[x][y+NE]) ? {x: x, y: y+NE} : undefined;
     } else if(dir === 'South') {
-        neighbor = MATROID[x] ? MATROID[x][y+SW] : undefined;
+        neighbor = (MATROID[x] && MATROID[x][y+SW]) ? {x: x, y: y+SW} : undefined;
     } else if(dir === 'East') {
-        neighbor = MATROID[x+NE] ? MATROID[x+NE][y] : undefined;
+        neighbor = (MATROID[x+NE] && MATROID[x+NE][y]) ? {x: x+NE, y: y} : undefined;
     } else if(dir === 'West') {
-        neighbor = MATROID[x+SW] ? MATROID[x+SW][y] : undefined;
+        neighbor = (MATROID[x+SW] && MATROID[x+SW][y]) ? {x: x+SW, y: y} : undefined;
     } else {
         throw new Error('Type Error: invalid neighbor-direction');
     }
-}
+    return neighbor;
+  }
 
-var Kin = function(coords){
+  var Kin = function(coords){
     var x = coords.x, y = coords.y
     this.neighbors = {
-        N: getNeighbor(x, y, 'North'),
-        S: getNeighbor(x, y, 'South'),
-        E: getNeighbor(x, y, 'East'),
-        W: getNeighbor(x, y, 'West')
+      N: getNeighborCoords(x, y, 'North'),
+      S: getNeighborCoords(x, y, 'South'),
+      E: getNeighborCoords(x, y, 'East'),
+      W: getNeighborCoords(x, y, 'West')
     };
-};
-var Person = function(person, coords){
+  };
+  var Person = function(person, coords){
     this.name = person.name;
     this.status = (person.alive + 0);
-    this.latLon = coords;
+    this.coords = coords;
     Kin.call(this, coords);
-};
+  };
 
-/**
- * Creates a matrix where:
- *  * X === array of:
- *  *   * Y === array of values
- * Usage: new Matroid(8, 8);
- */
-var Matroid = function(xLen, yLen){
-    const X = [], matroid = X;
+  /**
+   * Creates a matrix where:
+   *  * X === array of:
+   *  *   * Y === array of values
+   * Usage: new Matroid(8, 8);
+   */
+  var Matroid = function(xLen, yLen){
+    var x = [], matroid = x;
     BOARDX = xLen, BOARDY = yLen, AREA = (xLen * yLen);
     for(var i=0; i<yLen; i++){
-        var y = [];
-        for(var ii=0; ii<xLen; ii++){
-            y.push(-1);
-        }
-        X.push(y);
+      var y = [];
+      for(var ii=0; ii<xLen; ii++){
+          y.push(-1);
+      }
+      x.push(y);
     }
     MATROID = matroid;
     return matroid;
-};
+  };
 
-function createBoard(xLen, yLen){
+  function createBoard(xLen, yLen){
     return new Matroid(xLen, yLen);
-}
+  }
 
-function addMembers(members){
+  function addMembers(members){
     PARTICIPANTS = members.length;
     for(var i=0,l=members.length; i<l; i+=BOARDX){
-        var x = i/8;
-        console.log('@loops x', x);
-        for(var j=i,ll=i+BOARDY; j<ll; j++){
-            var y = j-x*BOARDY;
-            console.log('\t@loops y', j, y );
-            if(members[i] || members[j]){
-                MATROID[x][y] = new Person(members[j], {x: x, y: y});
-            }
+      var x = i/8;
+      for(var j=i,ll=i+BOARDY; j<ll; j++){
+        var y = j-x*BOARDY;
+        if(members[i] || members[j]){
+          var person = new Person(members[j], {x: x, y: y});
+          MATROID[x][y] = person;
+          MEMBERS.push(person);
         }
+      }
     }
-}
+  }
 
-function sendFlowersTo(name){
+  function getMember(coords){
+    if(!!coords === false){ return false; }
+    var region = coords.x * coords.y;
+    return (region >= 0 && region <= AREA) && MATROID[coords.x][coords.y];
+  }
+
+  function displayMembers(){
+    var tab = '<span style="margin-left: 12px;"></span>';
+    document.writeln('<br>');
+    for(var i=0,l=MATROID.length; i<l; i++){
+      document.writeln('<br>');
+      for(var j=0,m=MATROID[i].length; j<m; j++){
+        document.write(tab + MATROID[i][j].status);
+      }
+    }
+  }
+
+  function evolve(){
+    for(var i=0,l=MATROID.length; i<l; i++){
+      for(var j=0,m=MATROID[i].length; j<m; j++){
+        var person = MATROID[i][j], nghs = person.neighbors;
+        var sum = ( // use boolean-algebra
+          !!getMember(nghs.N).status
+          +
+          !!getMember(nghs.S).status
+          +
+          !!getMember(nghs.E).status
+          +
+          !!getMember(nghs.W).status
+        );
+        if(person.status === 1){  // live cell
+        
+          if(sum < 2){  // under-population
+            person.status = 0;
+          } else if(sum >= 2 && sum <= 3){  // servival
+            person.status = 1;
+          } else if(sum > 3){  // overcrowding
+            person.status = 0;
+          } else {
+            console.log('Evolution Error: miscalculated sum for live cell');
+          }
+          
+        } else if(person.status === 0){  // dead cell
+        
+          if(sum === 3){  // reproduction
+            person.status = 1;
+          } else {
+            // do nothing
+          }
+        } else {
+          console.log('@status', person.status);
+          throw new Error('Evolution Error: bad status');
+        }
+        
+      }
+    }
+  }
+
+  function rerender(){
+    var current = document.body.innerHTML;
+    displayMembers();
+    document.writeln('<br><br>');
+    document.writeln('<b>EVOLUTION:</b>');
+    document.body.innerHTML += current;
+  }
+
+  function sendFlowersTo(name){
     MEMBERS.forEach(function(membr){
-        if(membr.name === name){ membr.flowers = true; alert('flowers sent'); }
+      if(membr.name === name){ membr.flowers = true; alert('flowers sent'); }
     });
-}
+  }
 
-var board = createBoard(8, 8);
-addMembers(people);
+  function randomizeStatuses(ppl){
+    ppl.forEach(function(p){
+      p.status = Math.floor(Math.random()*2);
+    });
+  }
+  
+  return {
+    MEMBERS: MEMBERS,
+    newGame: createBoard,
+    addPeople: addMembers,
+    mixStatuses: randomizeStatuses,
+    sample: displayMembers,
+    evolve: evolve,
+    resample: rerender
+  };
+
+})();
+
+var board = Darwin.newGame(8, 8);
+Darwin.addPeople(people);
+Darwin.mixStatuses(Darwin.MEMBERS);
+Darwin.sample();
+setTimeout(function(){
+  Darwin.evolve();
+  Darwin.resample();
+}, (0*1000) );
 
 console.log('@board', board.length, board[0].length, board, board[0]);
 
